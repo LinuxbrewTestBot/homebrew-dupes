92cfb46f4ee6384352aea81d8f7933913694bf92
Alyssa Ross <hi@alyssa.is>
2016-09-11 18:38:22 +0000

    screen: use #append_to_cflags instead of #append

--- screen.rb
+++ screen.rb
@@ -46,7 +46,7 @@ class Screen < Formula
     # before osdef.sh script generates it.
     ENV.deparallelize
 
-    ENV.append "CFLAGS", "-include utmp.h"
+    ENV.append_to_cflags "-include utmp.h"
 
     system "./autogen.sh"
     system "./configure", "--prefix=#{prefix}",
