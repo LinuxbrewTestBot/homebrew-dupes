4774345205dedb3e42fbc0f9867130123faad195
Bob W. Hogg <rwhogg@linux.com>
2017-02-08 07:08:02 -0800

    tcpdump: update 4.9.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-dupes#105.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- tcpdump.rb
+++ tcpdump.rb
@@ -11,6 +11,7 @@ class Tcpdump < Formula
     sha256 "010248c6714481c459105589cefe45df08e429fcded0ca813ac4a39e3d526ba8" => :sierra
     sha256 "b2d53c0107757021c8ceaf1aab61b672dbdda78b8fccd93010cb012b0537f098" => :el_capitan
     sha256 "ff19d8db068534c16bf57b770c392b4c39d8c1a3a7fab48eef751f3413d86f5c" => :yosemite
+    sha256 "9c60551f91dfd899263ce3aa8fd1a987d6828f670834f0cf0270158df202a8e1" => :x86_64_linux
   end
 
   depends_on "openssl"
